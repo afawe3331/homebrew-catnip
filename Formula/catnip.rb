6f7b4dab90fc4ea5a20aa77780a78c2fd3d76462
afawe3331 <bichpul@naver.com>
2025-11-21 04:35:00 +0900

    Update catnip.rb

--- Formula/catnip.rb
+++ Formula/catnip.rb
@@ -3,13 +3,19 @@ class Catnip < Formula
   homepage "https://github.com/noriah/catnip"
   url "https://github.com/noriah/catnip/archive/refs/heads/master.zip"
   version "latest"
+  license "MIT"
 
   depends_on "go" => :build
   depends_on "fftw"
   depends_on "portaudio"
 
   def install
-    system "make"
-    bin.install "bin/catnip"
+    # Go 빌드: ./cmd/catnip 경로에 메인 소스가 있음
+    system "go", "build", *std_go_args(output: bin/"catnip"), "./cmd/catnip"
+  end
+
+  test do
+    # 설치 확인: 실행 파일이 정상 동작하는지 체크
+    assert_match "Usage", shell_output("#{bin}/catnip --help", 1)
   end
 end
